8c405289d5e15fc0bd93b894d0394dc3960269f9
rifatmahmud21 <rifatmahmud2712@gmail.com>
2022-01-26 23:53:35 +0600

    3rd

--- 1st-day.js
+++ 1st-day.js
@@ -1,1 +1,18 @@
+// number type variable
 var price = 30;
+var age = 21;
+var temp = 67;
+
+
+// String type variable
+var names = "sifat mahmud";
+var names = 'miraj uddin';
+var school = "Light house lab. school and college";
+
+
+// true/false or boolean variable
+var pass = true;
+var subscribe = false;
+
+
+
